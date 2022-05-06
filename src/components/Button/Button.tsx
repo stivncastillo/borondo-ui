ce492af02bf9030103a124309c67a8f058944c66
Stiven <stivencastillo.90@gmail.com>
2022-05-06 10:29:12 -0500

    Create project

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Button = (props: Props) => {
+  return <div className="bg-red-500 text-white py-2 px-4">Button</div>;
+};
+
+export default Button;
